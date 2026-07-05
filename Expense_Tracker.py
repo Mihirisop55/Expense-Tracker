f9f66cb67c336b60ec1663e55ebed196a3d3b40b
Mihir <119856792+Mihirisop55@users.noreply.github.com>
2026-07-05 12:26:54 +0530

    Enhance Expense Tracker with date and CSV export

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -5,13 +5,16 @@ import pandas
 
 Expense = []
 Amount = []
+Date = []
 money=True
 
-print("---- Expense Tracker ----")
+print("------ Expense Tracker -------")
 print("1. Add New expense (press 1)")
 print("2. View all expense (press 2)")
 print("3. View summary (press 3)")
-print("4. Exit (press 4)")
+print("4. Get CSV file (press 4)")
+print("5. Exit (press 5)")
+print("------------------------------")
 while money:
     try:
         reply = int(input("Enter your choice: "))
@@ -19,24 +22,45 @@ while money:
         print("Invalid choice")
         print("Please enter a valid choice")
     else:
-        if reply<1 or reply>4:
+        if reply<1 or reply>5:
             print("Invalid choice")
             print("Please enter a valid choice")
+            print()
         elif reply==1:
             expense = input("Enter the expense: ")
             amount = int(input("Enter the amount: "))
+            date = input("Enter the date (YYYY-MM-DD): ")
             Expense.append(expense)
             Amount.append(amount)
+            Date.append(date)
             print("Your Expense has been added successfully")
-        elif reply==2:
-            data = {"Expense": Expense, "Amount": Amount}
+            print()
+        elif reply==2 and len(Expense)==0:
+            print("Please add some expenses to use this feature")
+            print()
+        elif reply==2 and len(Expense)>0:
+            data = {"Date": Date, "Expense": Expense, "Amount": Amount}
             df = pandas.DataFrame(data)
             print(df)
-        elif reply==3:
+            print()
+        elif reply==3 and len(Expense)==0:
+            print("Please add some expenses to use this feature")
+            print()
+        elif reply==3 and len(Expense)>0:
             print("Here is a Pie Chart for your expenses")
             plt.pie(Amount, labels=Expense, autopct='%1.1f%%')
             plt.title("Expense Summary")
+            plt.fontweight = 'bold'
             plt.show()
-        elif reply==4:
+            print()
+        elif reply==4 and len(Expense)==0:
+            print("Please add some expenses to use this feature")
+            print()
+        elif reply==4 and len(Expense)>0:
+            df = pandas.DataFrame({"Expense": Expense, "Amount": Amount, "Date": Date})
+            df.to_csv("Expenses.csv", index=False)
+            print("CSV file generated successfully")
+            print()
+        elif reply==5:
             print("Thank you for using the Expense Tracker")
             money=False
